7847b19b0651db2460466738fa1c446b3b110b9e
garj <johngargalionis@gmail.com>
2018-01-24 00:24:13 +1100

    Finished add

--- xarta/commands/add.py
+++ xarta/commands/add.py
@@ -2,17 +2,27 @@
 
 
 from json import dumps
+import os
 
 from .base import Base
 from ..utils import arxiv_open
+from ..database import PaperDatabase
 
+HOME = os.path.expanduser('~')
 
 class Add(Base):
-    """Add an arXiv paper and its metadata to the database."""
+    """ Add an arXiv paper and its metadata to the database. """
 
     def run(self):
-        print('Hello, world!')
         options = self.options
-        print(options)
         ref = options['<ref>']
         tags = options['--tag']
+
+        try:
+           with open(HOME+'/.xarta', 'r') as xarta_file:
+               database_path = xarta_file.readline()
+        except:
+            raise Exception('Something went wrong...') # TODO make this error more explicit
+
+        paper_database = PaperDatabase(database_path)
+        paper_database.add_paper(paper_id=ref, tags=tags)
