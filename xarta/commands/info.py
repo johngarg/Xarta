6d33aaf8cdd6ea1d5df2d656edc3563fad1e0010
John Gargalionis <johngargalionis@gmail.com>
2018-09-10 20:46:49 +1000

    Added all paper information to info

--- xarta/commands/info.py
+++ xarta/commands/info.py
@@ -2,10 +2,10 @@
 
 
 from json import dumps
-import pprint
 
 from .base import Base
-from ..utils import get_arxiv_data
+from ..utils import get_arxiv_data, read_xarta_file
+from ..database import PaperDatabase
 
 
 class Info(Base):
@@ -14,5 +14,15 @@ class Info(Base):
     def run(self):
         options = self.options
         ref = options['<ref>']
-        info = get_arxiv_data(ref)
-        pprint.pprint(info)
+
+        database_path = read_xarta_file()
+        paper_database = PaperDatabase(database_path)
+        info = paper_database.query_papers_contains(
+            paper_id=ref, title=None, author=None, category=None, tags=[], filter=None, silent=True)
+
+        info = info[0]
+        print(f'arXiv Ref: {info[0]}')
+        print(f'Title: {info[1]}')
+        print(f'Authors: {info[2]}')
+        print(f'Category: {info[3]}')
+        print(f'Tags: {info[4]}')
