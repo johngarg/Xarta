cc41aebddc40eaf645d41c0d7c702f8a0e30396f
John Gargalionis <johngargalionis@gmail.com>
2018-10-06 02:57:41 +1000

    Formatting improvements

--- xarta/database.py
+++ xarta/database.py
@@ -1,6 +1,6 @@
 import sqlite3
 import os
-from .utils import get_arxiv_data, list_to_string, expand_tag, format_data_term
+from .utils import get_arxiv_data, list_to_string, expand_tag, format_data_term, get_all_rows_from_db
 
 HOME = os.path.expanduser('~')
 
@@ -11,19 +11,23 @@ class PaperDatabase():
     def create_connection(self):
         """Create a database connection to a SQLite database."""
         print("Creating new database at " + self.path + "...")
+
         conn = sqlite3.connect(self.path)
         conn.close()
+
         with open(HOME+'/.xarta', 'w') as xarta_file:
             xarta_file.write(self.path)
 
     def initialise_database(self):
-        """Initialise database with empty table.
-        """
+        """Initialise database with empty table."""
+        init_command = '''CREATE TABLE papers
+                          (id text, title text, authors text, category text, tags text);'''
+
         conn = sqlite3.connect(self.path)
         with conn:
             print("Initialising database...")
-            conn.execute('''CREATE TABLE papers
-                         (id text, title text, authors text, category text, tags text);''')
+            conn.execute(init_command)
+
         print('Database initialised!')
 
     def add_paper(self, paper_id, tags):
@@ -36,12 +40,13 @@ class PaperDatabase():
             author_string = list_to_string(data['authors'])
             tags = [expand_tag(tag, data) for tag in tags]
             tags = list_to_string(tags)
-
             insert_command = f'''INSERT INTO papers
                                  (id, title, authors, category, tags)
                                  VALUES
                                  ("{paper_id}", "{data['title']}", "{author_string}", "{data['category']}", "{tags}");'''
+
             conn.execute(insert_command)
+
         print(f"{paper_id} added to database!")
 
     def delete_paper(self, paper_id):
@@ -50,6 +55,7 @@ class PaperDatabase():
         with conn:
             delete_command = f'''DELETE FROM papers WHERE id = "{paper_id}";'''
             conn.execute(delete_command)
+
         print(f"{paper_id} deleted from database!")
 
     def edit_paper_tags(self, paper_id, new_tags):
@@ -57,20 +63,15 @@ class PaperDatabase():
         conn = sqlite3.connect(self.path)
         with conn:
             new_tags = list_to_string(new_tags)
-
             edit_tags_command = f'''UPDATE papers SET tags = "{new_tags}"
                                     WHERE id = "{paper_id}";'''
             conn.execute(edit_tags_command)
+
         print(f"{paper_id} now has the following tags in the database: {new_tags}")
 
     def query_papers(self, silent=False):
         """Query information about a paper in the database."""
-        conn = sqlite3.connect(self.path)
-        c = conn.cursor()
-        query_command = f'''SELECT * FROM papers;'''
-        query_results = c.execute(query_command)
-        all_rows = c.fetchall()
-        c.close()
+        all_rows = get_all_rows_from_db(self.path)
 
         # get current console window dimensions
         data = format_data_term(all_rows)
@@ -87,15 +88,13 @@ class PaperDatabase():
     def query_papers_contains(
             self, paper_id, title, author, category, tags, filter,
             silent=False, select=False):
-        """
-        Function to search and filter paper database. Returns a list of
+        """Function to search and filter paper database. Returns a list of
         tuples and (if `silent` is False) prints a table to the screen. Search
         parameters connected by a logical OR, thus:
-           db.query_papers_contains(paper_id=None,
-                                    title=None,
-                                    author='Weinberg',
-                                    category='hep-th',
-                                    tags=[])
+
+            db.query_papers_contains(paper_id=None, title=None, author='Weinberg',
+                                     category='hep-th', tags=[])
+
         will return every paper in the database from 'hep-th' as well as those
         by 'Weinberg'.
         """
@@ -140,9 +139,8 @@ class PaperDatabase():
         return data
 
     def contains(self, ref):
-        """
-        Returns a boolean identifying if an entry with reference `ref` exists
-        within the database.
+        """Returns a boolean identifying if an entry with reference `ref`
+        exists within the database.
         """
         search_results = self.query_papers_contains(
             paper_id=ref,
