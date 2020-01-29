d7ea115547ab62289d0df3cc6f44e78aa12354fb
Leon Friedrich <leonsfriedrich@gmail.com>
2020-01-29 19:17:19 +1100

    Xarta init now expands relative paths, i.e, 'xarta init ./'

--- xarta/commands/init.py
+++ xarta/commands/init.py
@@ -4,20 +4,25 @@
 from .base import Base
 from ..database import PaperDatabase
 
+
 class Init(Base):
     """ Initialise database """
 
     def run(self):
         import os
 
-        database_location = self.options['<database-location>'] + ".xarta.d/"
+        database_location = self.options["<database-location>"] + ".xarta.d/"
         database_path = database_location + "db.sqlite3"
+
+        # resolve relative paths, e.g., 'xarta init ./'
+        database_path = os.path.abspath(database_path)
+
         os.makedirs(database_location, exist_ok=True)
 
         # TODO Write some code to allow reinit of database to different location
         # and update of .xarta file accordingly
 
-        if 'db.sqlite3' not in os.listdir(database_location):
+        if "db.sqlite3" not in os.listdir(database_location):
             sql_command = "sqlite3 " + database_path + ' ";"'
             os.system(sql_command)
             paper_database = PaperDatabase(database_path)
